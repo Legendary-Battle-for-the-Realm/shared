1b69b81349d1e11fd3fe4a6484632983d18f1b71
TKira4 <toan01224551919@gmail.com>
2025-04-03 17:10:15 +0700

    Create User_Model.cs

---
+++ DataModels/User_Model.cs
@@ -0,0 +1,9 @@
+public class User
+    {
+        [Key]
+        public required int UserId { get; set; } 
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+        public int? HealthPoints { get; set; }
+    }
