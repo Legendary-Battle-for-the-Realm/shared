89047aed316dd79f6fc74ce9827b239822f92710
TKira4 <toan01224551919@gmail.com>
2025-04-03 17:24:06 +0700

    Update EquimentSkill.cs

--- DataModels/EquimentSkill.cs
+++ DataModels/EquimentSkill.cs
@@ -1,5 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 public class EquipmentSkill
 {
+    [Key] 
+    public required int Id { get; set; } 
+
     public required string Name { get; set; }
     public required string Ref { get; set; }
 }
