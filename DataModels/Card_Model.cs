7b9a7f65753dfd8bdb7e3a3066a2caf3544a41ac
TKira4 <toan01224551919@gmail.com>
2025-04-03 17:19:08 +0700

    Update Card_Model.cs

--- DataModels/Card_Model.cs
+++ DataModels/Card_Model.cs
@@ -18,7 +18,7 @@ public class Card
     public required CardType Type { get; set; } 
 
     public int? EffectId { get; set; } 
-    public Effect Effect { get; set; } 
+    public Effect? Effect { get; set; } 
 
     public int? Quantity { get; set; } 
 }
