4793dd4185b33acef8bbdcc39a9f2fa6d4d63a07
TKira4 <toan01224551919@gmail.com>
2025-04-12 21:04:58 +0700

    Create Room.cs

---
+++ DataModels/Room.cs
@@ -0,0 +1,14 @@
+namespace Shared.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public int MaxPlayers { get; set; } 
+        public List<Player> Players { get; set; } = new List<Player>(); 
+        public bool IsGameStarted { get; set; } 
+        public int CurrentTurnPlayerId { get; set; } 
+        public List<Card> Deck { get; set; } = new List<Card>(); 
+        public List<int> TurnOrder { get; set; } = new List<int>(); 
+    }
+}
