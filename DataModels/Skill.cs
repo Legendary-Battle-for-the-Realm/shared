768a9bbf9989660cd673d097fe397322fec6275f
TKira4 <toan01224551919@gmail.com>
2025-04-12 20:35:48 +0700

    thêm player để quản lí người chơi

--- DataModels/Skill.cs
+++ DataModels/Skill.cs
@@ -7,7 +7,6 @@ namespace Shared.Models
         public string Name { get; set; } = null!;
         public string Ref { get; set; } = null!;
         public int Cost { get; set; }
-        // Thuộc tính bổ sung từ skill.json (nếu cần lưu vào database)
         public required string Desc { get; set; }
         public required string Effect { get; set; }
     }
