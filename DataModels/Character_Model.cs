b8dc7af0c148dff59c046514df22b983e7b48b48
TKira4 <toan01224551919@gmail.com>
2025-04-13 16:21:50 +0700

    Update Character_Model.cs

--- DataModels/Character_Model.cs
+++ DataModels/Character_Model.cs
@@ -5,16 +5,15 @@ namespace Shared.Models
 {
     public class Character
     {
-        [Key]
-        public required int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Desc { get; set; }
-        public required int Atk { get; set; }
-        public required string Faction { get; set; }
-        public required string Cultivation { get; set; }
-        public required int HP { get; set; } = 100;
-        public required int Qi { get; set; } = 0;
-        public required List<Skill> Skills { get; set; } = new List<Skill>();
-        public required int UserId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Desc { get; set; }
+        public string Faction { get; set; } = string.Empty;
+        public string Cultivation { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public int HP { get; set; }
+        public int Qi { get; set; }
+        public int Atk { get; set; }
+        public List<Skill> Skills { get; set; } = new List<Skill>();
     }
 }
