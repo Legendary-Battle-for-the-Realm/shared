df2058e1419b9be2f85ac42cf67951adec7c1898
TKira4 <toan01224551919@gmail.com>
2025-04-15 15:33:47 +0700

    sửa kiểu dữ liệu

--- DataModels/Armor_Model.cs
+++ DataModels/Armor_Model.cs
@@ -10,8 +10,8 @@ namespace Shared.Models
         [Key]
         public required int Id { get; set; }
         public required string Name { get; set; }
-        public required string Atk { get; set; }
-        public required string Def { get; set; }
+        public required int Atk { get; set; }
+        public required int Def { get; set; }
         public required string Desc { get; set; }
         public int? UserId { get; set; }
         public required string CultivationRequired { get; set; }
--- DataModels/Weapon_Model.cs
+++ DataModels/Weapon_Model.cs
@@ -10,7 +10,7 @@ namespace Shared.Models
         [Key]
         public required int Id { get; set; }
         public required string Name { get; set; }
-        public required string Atk { get; set; }
+        public required int Atk { get; set; }
         public required string Desc { get; set; }
         public int? UserId { get; set; }
         public required string CultivationRequired { get; set; }
